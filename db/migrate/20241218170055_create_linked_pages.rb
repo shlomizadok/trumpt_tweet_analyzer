478c51d5ae35c96738c007d005543b0f322299a9
Shlomi Zadok <shlomi.zadok@yahooinc.com>
2024-12-22 09:15:02 +0200

    Remove content (not used), add index

--- db/migrate/20241218170055_create_linked_pages.rb
+++ db/migrate/20241218170055_create_linked_pages.rb
@@ -2,10 +2,11 @@ class CreateLinkedPages < ActiveRecord::Migration[8.0]
   def change
     create_table :linked_pages do |t|
       t.string :url
-      t.text :content
       t.references :tweet, null: false, foreign_key: true
 
       t.timestamps
     end
+    
+    add_index :linked_pages, :url, unique: true
   end
 end
